795c5af556a03fe2c2e54c283378fa7a6633e5be
dhalmos-ucsd <dhalmos@ucsd.edu>
2023-09-18 23:52:57 +0000

    Fixed syntax errors

--- bootcamp/contrib/student_18.py
+++ bootcamp/contrib/student_18.py
@@ -1,5 +1,7 @@
 from bootcamp.core.student_18 import count_substring  # noqa
-def count_substring_but_i_express_pain(string, substring):
+
+
+def count_substring_with_pain(string, substring):
     hurt = "OW GAH OOF EEEEYYYOOOO AAAGGGHHHH OUCHTHATHURT"
     painpoint = count_substring(string, substring)
     return hurt.split(' ')[painpoint]
