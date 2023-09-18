962f419b26da7c4377633e9f1c7fb53adb66e6c7
eup009 <eup009@ucsd.edu>
2023-09-18 23:17:56 +0000

    ENH make count_substring case insensitive

--- bootcamp/core/student_17.py
+++ bootcamp/core/student_17.py
@@ -14,6 +14,8 @@ def count_substring(string, substring):
         The number of times `substring` occurs in `string`
 
     """
+    string = string.lower()
+    substring = substring.lower()
     count = 0
 
     string_length = len(string)
