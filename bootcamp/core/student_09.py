836d35a7fbeec1929b4a9d12a2f3b1c8120661a0
heg003 <heg003@ucsd.edu.com>
2023-09-18 23:41:33 +0000

    some init msg

--- bootcamp/core/student_09.py
+++ bootcamp/core/student_09.py
@@ -19,12 +19,18 @@ def count_substring(string, substring):
     string_length = len(string)
     substring_length = len(substring)
     n_subsequences = string_length - substring_length + 1
-
+    
+    dict = {"A":"a", "T":"t", "C":"c", "G":"g"}
+    
     for i in range(n_subsequences):
         left_bound = i
         right_bound = i + substring_length
         candidate_substring = string[left_bound:right_bound]
         if candidate_substring == substring:
             count += 1
-
+        else for j in range(substring_length):
+            nucleotide = {n for n in dic if dic[subtring[j]]==subtring[j]}
+            candidate_substring = can
     return count
+
+
