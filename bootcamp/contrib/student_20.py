817f8acc1e865d462f235875a953aa5253d98d1a
kputato <kcpu@ucsd.edu>
2024-09-19 17:49:45 +0000

    changes to contribute

---
+++ bootcamp/contrib/student_20.py
@@ -0,0 +1,17 @@
+def len_substring(string):
+    """Calculates length of the string
+
+    Parameters
+    ----------
+    string : str
+        The string where length is calculated
+
+    Returns
+    -------
+    int
+        The length of the string
+
+    """
+    
+    string_length = len(string)
+    return string_length
