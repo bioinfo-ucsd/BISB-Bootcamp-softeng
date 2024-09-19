8e76dec7c89f02dadcf65f79d6eeb0a39bf847a7
lauhino <lhinojosa@ucsd.edu>
2024-09-19 18:00:32 +0000

    Add contribution and corresponding test function.

--- bootcamp/core/tests/test_student_22.py
+++ bootcamp/core/tests/test_student_22.py
@@ -1,4 +1,5 @@
 from bootcamp.core.student_22 import count_substring  # noqa
+from bootcamp.contrib.student_22 import sequence_fasta
 
 
 def test_count_substring_single():
@@ -26,3 +27,12 @@ def test_count_substring_none():
     expected_count = 0
     observed_count = count_substring(test_string, test_substring)
     assert expected_count == observed_count
+
+
+def test_fasta_fun():
+    test_string = "agtcccctaga"
+    test_name = "test1"
+
+    expected_out = ">test1\nagtcccctaga\n"
+    observed_out = sequence_fasta(sequence=test_string, name=test_name)
+    assert expected_out == observed_out
