8c7f9ebf174dddc5093ea8951d0e656230fef4f3
Jeremy Parker Yang <jparkeryang@ucsd.com>
2024-09-19 17:40:22 +0000

    ENH make count_substring case insensitive

--- bootcamp/core/tests/test_student_16.py
+++ bootcamp/core/tests/test_student_16.py
@@ -1,6 +1,33 @@
 from bootcamp.core.student_16 import count_substring  # noqa
 
 
+def test_count_substring_single_case_insensitive():
+    test_string = "CGCTAGCGT"
+    test_substring = "tag"
+
+    expected_count = 1
+    observed_count = count_substring(test_string, test_substring)
+    assert expected_count == observed_count
+
+
+def test_count_substring_repeated_case_insensitive():
+    test_string = "AGCTAGCAGT"
+    test_substring = "agc"
+
+    expected_count = 2
+    observed_count = count_substring(test_string, test_substring)
+    assert expected_count == observed_count
+
+
+def test_count_substring_none_case_insensitive():
+    test_string = "AGTCCCCTAGA"
+    test_substring = "aaa"
+
+    expected_count = 0
+    observed_count = count_substring(test_string, test_substring)
+    assert expected_count == observed_count
+
+
 def test_count_substring_single():
     test_string = "CGCTAGCGT"
     test_substring = "TAG"
